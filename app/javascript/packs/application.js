db7f03747cfa9165728c27a2bc66b1b83c89e2b5
Sian Stone <sian.m.stone@gmail.com>
2019-11-26 16:56:32 +0000

    profile booking styles

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,9 @@ import "bootstrap";
 import { initMapbox } from '../plugins/init_mapbox';
 import { toggleDateInputs } from '../plugins/init_datepicker';
 import { toggleDogSelect } from '../plugins/dog_booking';
+import { hoverReview } from '../plugins/profile_booking_page'
 
 initMapbox();
 toggleDateInputs();
 toggleDogSelect();
+hoverReview();
